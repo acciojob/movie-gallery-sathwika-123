fa4a940816e4da1da7d51e7874058db32c19d799
sathwika-123 <147599064+sathwika-123@users.noreply.github.com>
2023-11-14 15:23:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your code here
+// script.js - Your Custom JavaScript Code here
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Your JavaScript code that should run after the DOM is fully loaded goes here
+    console.log("DOM is ready!");
+});
+
